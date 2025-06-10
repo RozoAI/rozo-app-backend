e1c2a2b80b57b4f2916f461c0d1b871b2bcad125
Devendra116 <chauhandevendra116@gmail.com>
2025-06-10 23:06:20 +0530

    add languages and currencies

---
+++ supabase/migrations/20250610173326_add_languages_currencies.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.languages (language_id, display_name) VALUES
+('ZH', 'Chinese (Mandarin)'),
+('HI', 'Hindi'),
+('ES', 'Spanish'),
+('FR', 'French'),
+('AR', 'Arabic'),
+('BN', 'Bengali'),
+('RU', 'Russian'),
+('PT', 'Portuguese'),
+('ID', 'Indonesian');
+
+INSERT INTO "public"."currencies" ("currency_id", "display_name") VALUES 
+('IDR', 'Indonesian Rupiah'),  
+('SGD', 'Singapore Dollar'), 
+('USD', 'United States Dollar');
